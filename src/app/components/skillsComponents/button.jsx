e7445536d6017117543a6f66c8de35fa2c6b60e4
Breno Henrique <brenoeevil@gmail.com>
2025-12-09 11:49:12 -0300

    feat(mobile): Section skills no mobile finalizada, Próximo passo: seção Projects mobile

---
+++ src/app/components/skillsComponents/button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <>
+      <div className="flex justify-center font-space ">
+        <div className="bg-[#FDF5E6] p-2 px-6 rounded-lg text-2xl">
+          View All
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Button;
--- src/app/sections/skillsSCROLL/skillsSection.jsx
+++ src/app/sections/skillsSCROLL/skillsSection.jsx
@@ -1,10 +1,31 @@
+import Cards from "@/app/components/skillsComponents/cards";
 import React from "react";
+import ReactIcon from "../../assets/reacticon.png";
+import JsIcon from "../../assets/jsicon.png";
+import HtmlIcon from "../../assets/htmlicon.png";
+import CssIcon from "../../assets/cssicon.png";
+import Title from "@/app/components/skillsComponents/title";
+import NextIcon from "../../assets/nexticon.png";
+import TailwindIcon from "../../assets/tailwindicon.png";
+import Button from "@/app/components/skillsComponents/button";
 
 const SkillsSection = () => {
   return (
-    <div>
-      <h1>Skills</h1>
-    </div>
+    <section className="bg-[#0D182A] min-h-screen flex flex-col items-center gap-y-6">
+      <Title />
+
+      <div className="grid grid-cols-2 gap-2">
+        <Cards icon={HtmlIcon} name={"HTML"} />
+        <Cards icon={CssIcon} name={"CSS"} />
+        <Cards icon={JsIcon} name={"JS"} />
+        <Cards icon={ReactIcon} name={"REACT"} />
+        <Cards icon={NextIcon} name={"NEXT"} />
+        <Cards icon={TailwindIcon} name={"TAILWIND"} />
+      </div>
+      <div className="py-4">
+        <Button />
+      </div>
+    </section>
   );
 };
 
